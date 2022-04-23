d48d3813c22b095554bfd40984697253d61e4586
Трапеза Игорь В <trapezaiv@lab227.local>
2022-04-23 13:03:12 +0400

    Task04
    Task04

---
+++ Task04/src/run_test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace rmvit\task04;
+
+require __DIR__ . '/../vendor/autoload.php';
+require 'StackInterface.php';
+require 'Stack.php';
+require 'compareStrings.php';
+require 'Test.php';
+
+run_test();
